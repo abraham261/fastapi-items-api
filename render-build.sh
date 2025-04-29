8c16518bbd61067b46918b2ff4ee4296a1039a97
abraham261 <abrahamleonard331@gmail.com>
2025-04-29 16:54:10 +0200

    Fix définitif du script build pour Render

---
+++ render-build.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/sh
+echo '=== Début du build Render ==='
+ls -la
+echo '=== Vérification requirements.txt ==='
+find . -name requirements.txt
+echo '=== Installation des dépendances ==='
+pip install --upgrade pip
+pip install -r requirements.txt
